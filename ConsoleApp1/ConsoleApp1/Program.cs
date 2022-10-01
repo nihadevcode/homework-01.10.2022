7449e96999f2ecbf28bb34234921b5ee4de7c112
Nihad Valiyev <nihadev@code.edu.az>
2022-10-02 02:01:05 +0400

    adding file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "n";
+            int x = 2;
+            int y = 3;
+            int z = 5;
+            int c = 7;
+
+            int n = 123;
+
+            if (n % x == 0 || n % y == 0 || n % z == 0 || n % c == 0)
+            {
+                Console.WriteLine("Mürəkkəb ədəddir");
+            }
+            else
+            {
+                Console.WriteLine("Sadə ədəddir");
+            }
+        }
+
+                
+        }
+    }
+}
